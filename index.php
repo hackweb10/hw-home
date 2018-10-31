bc4c136230af4a08d527902705f413eccb32142a
frankie <frank10gm@gmail.com>
2018-10-31 14:19:24 +0100

    ok

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-echo 'hw-home';
-phpinfo();
+echo '# hw-home #';
 ?>
+<br>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"].'/wake.php');?>">  
+Name: 
+<input type="text" name="c" value="">
+<input type="submit" name="submit" value="send"> 
+</form>
