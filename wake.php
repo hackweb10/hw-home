1e42274b55fb3ab4f004af386faa7372ede20d64
frank10gm <frank10gm@gmail.com>
2018-10-31 12:54:26 +0100

    Update wake.php

--- wake.php
+++ wake.php
@@ -2,7 +2,7 @@
 namespace hwHome;
 class WakeOnLAN
 {
-    public static function wakeUp($macAddressHexadecimal, $broadcastAddress)
+    public static function wakeUp2($macAddressHexadecimal, $broadcastAddress)
     {
         $macAddressHexadecimal = str_replace(':', '', $macAddressHexadecimal);
         // check if $macAddress is a valid mac address
@@ -18,5 +18,39 @@ class WakeOnLAN
         fclose($fp);
         echo 'woke up';
     }
+    
+    public static function wakeUp($mac, $addr){
+        $addr_byte = explode(':', $mac);
+        $hw_addr = '';
+
+        for ($a=0; $a < 6; $a++) $hw_addr .= chr(hexdec($addr_byte[$a]));
+
+        $msg = chr(255).chr(255).chr(255).chr(255).chr(255).chr(255);
+
+        for ($a = 1; $a <= 16; $a++) $msg .= $hw_addr;
+
+        // send it to the broadcast address using UDP
+        // SQL_BROADCAST option isn't help!!
+        $s = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        if ($s == false)
+        {
+            echo "Error creating socket!\n";
+            echo "Error code is '".socket_last_error($s)."' - " . socket_strerror(socket_last_error($s));
+        }
+        else
+        {
+            // setting a broadcast option to socket:
+            $opt_ret = socket_set_option($s, 1, 6, TRUE);
+            if($opt_ret < 0)
+            {
+                echo "setsockopt() failed, error: " . strerror($opt_ret) . "\n";
+            }
+            $e = socket_sendto($s, $msg, strlen($msg), 0, $addr, 2050);
+            echo $e;
+            socket_close($s);
+            echo "Magic Packet sent (".$e.") to ".$addr.", MAC=".$mac;
+        }
+    } 
+    
 }
 ?>
