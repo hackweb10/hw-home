d746d1caa09a4a2fe734377efe5644f9585562aa
frankie <frank10gm@gmail.com>
2018-10-31 14:23:42 +0100

    ok

--- wake.php
+++ wake.php
@@ -1,6 +1,13 @@
 <?php
 require_once __DIR__.'/wakeService.php';
 //echo __DIR__.'/wake.php';
-if(!isset($_POST['c'])) die('not set');
-if($_POST['c'] == '001') \hwHome\WakeOnLAN::wakeUp('90:e6:ba:ee:d1:91', '192.168.1.255');
+if(!isset($_POST['c'])) die('"c" not found.');
+switch($_POST['c']){
+    case '001':
+    \hwHome\WakeOnLAN::wakeUp('90:e6:ba:ee:d1:91', '192.168.1.255');
+    break;
+    default:
+    echo $_POST['c'].' not found.';
+    break;
+}
 ?>
