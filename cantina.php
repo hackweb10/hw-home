98ee61349a7d7190df3ce8cc2e67db54f142bae9
frankie <frank10gm@gmail.com>
2018-10-31 12:59:33 +0100

    ok

--- cantina.php
+++ cantina.php
@@ -1,5 +1,5 @@
 <?php
 require_once __DIR__.'/wake.php';
 //echo __DIR__.'/wake.php';
-if($_GET['computer'] == 'cantina') \hwHome\WakeOnLAN::wakeUp('90:e6:ba:ee:d1:91', '192.168.1.255');
+if($_GET['c'] == '001') \hwHome\WakeOnLAN::wakeUp('90:e6:ba:ee:d1:91', '192.168.1.255');
 ?>
