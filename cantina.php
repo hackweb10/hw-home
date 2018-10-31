31cfdb60e3fd644d1d89ab7c152ced9cc7947cf7
frank10gm <frank10gm@gmail.com>
2018-10-31 12:37:55 +0100

    Update cantina.php

--- cantina.php
+++ cantina.php
@@ -1,5 +1,5 @@
 <?php
-//require_once __DIR__.'/wake.php';
-echo __DIR__.'/wake.php';
-//\PHPGangsta\WakeOnLAN::wakeUp('90:e6:ba:ee:d1:91', '192.168.1.255');
+require_once __DIR__.'/wake.php';
+//echo __DIR__.'/wake.php';
+\hwHome\WakeOnLAN::wakeUp('90:e6:ba:ee:d1:91', '192.168.1.255');
 ?>
