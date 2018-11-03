44e58f9d4bebe4f12a46b8eb7a7e9a4de7477185
frankie <frank10gm@gmail.com>
2018-11-03 14:15:25 +0100

    ok

--- hwHome/index.php
+++ hwHome/index.php
@@ -3,7 +3,7 @@ echo '<strong># hw-home #</strong>';
 ?>
 <br>
 <br>
-<form method="post" action="<?php echo htmlspecialchars(__DIR__.'/wake.php');?>">  
+<form method="post" action="<?php echo htmlspecialchars('/hwHome/wake.php');?>">
 Name: 
 <input type="text" name="c" value="">
 <input type="submit" name="submit" value="send"> 
