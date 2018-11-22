2252573368d06fe572161112a503ffb8c296582a
frankie <frank10gm@gmail.com>
2018-11-22 14:11:48 +0100

    ok

--- hwHome/index.php
+++ hwHome/index.php
@@ -1,14 +1,24 @@
-<?php
-//require_once('./wake.php');
-echo '<strong># hw-home #</strong>';
-?>
-<br>
-<br>
-<form method="post" action="<?php echo htmlspecialchars('/hwHome/');?>">
-Name: 
-<input type="text" name="c" value="">
-<input type="submit" name="submit" value="send"> 
-</form>
-<?php
-require_once('./wake.php');
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<h1><strong># hwHome #</strong></h1>
+<p>
+    Turner on.
+    <br>
+    <br>
+    <form method="post" action="<?php echo htmlspecialchars('/hwHome/');?>">
+    Name: 
+    <input type="text" name="c" value="">
+    <input type="submit" name="submit" value="send"> 
+    </form>
+    <?php
+    require_once('./wake.php');
+    ?>
+</p>
+
+</body>
+</html>
