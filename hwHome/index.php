0d9f67f279213cf31ea27999c5b33271b6d21b7b
frankie <frank10gm@gmail.com>
2018-11-22 14:25:24 +0100

    ok

--- hwHome/index.php
+++ hwHome/index.php
@@ -5,7 +5,34 @@
 <title>hwHome</title>
 <style>
 input[type="text"] {
-  width: 100%;
+    width: 100%;
+    background-color: transparent;
+    border: none;
+    margin: 0;
+    padding: 0;
+    color: rgba(0,0,0,.87);
+    word-wrap: break-word;
+    outline: none;
+    display: flex;
+    flex: 100%;
+    -webkit-tap-highlight-color: transparent;
+    margin-top: -37px;
+}
+.SDkEP {
+    flex: 1;
+    display: flex;
+    padding: 5px 8px 0 16px;
+}
+.RNNXgb {
+    background: #fff;
+    display: flex;
+    border-radius: 2px;
+    border: none;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08);
+    z-index: 3;
+    height: 44px;
+    margin: 0 auto;
+    max-width: 484px;
 }
 </style>
 </head>
