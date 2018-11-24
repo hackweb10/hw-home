d940644b61713c6988bdec2c2f5791e70f78f4df
frankie <frank10gm@gmail.com>
2018-11-24 20:41:04 +0100

    ok

--- hwHome/hibernate.php
+++ hwHome/hibernate.php
@@ -1,7 +1,7 @@
 <?php
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL,"http://192.168.1.109/hackweb/hw_web/www/api/hibernate.php");
+curl_setopt($ch, CURLOPT_URL,"http://192.168.1.109/hibernate.php");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,  http_build_query(array('postvar1' => 'value1', 'postvar2' => 'value2')));
 
