fe3403d6dbf8f5fc514002db769d24a2d571f491
frankie <frank10gm@gmail.com>
2018-11-25 08:38:03 +0100

    ok

--- hwHome/ping.php
+++ hwHome/ping.php
@@ -1,5 +1,5 @@
 <?php
-$timeout = 5;
+$timeout = 1;
 $socket = @fsockopen( '192.168.1.109', 80, $errno, $errstr, $timeout );
 $online = ( $socket !== false );
 if($online) echo '005 is <span style="color:green;">online</span> <button class="btn" id="hibernate">Hibernate</button>';
